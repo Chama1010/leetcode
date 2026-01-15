e270932840a7848e7bb8cece32e47bdfb9091be4
Chama1010 <ashenchamath1010@GMAIL.com>
2026-01-15 13:31:47 +0530

    Slove 01.two sum

---
+++ arrays/01_two_sum.py
@@ -0,0 +1,10 @@
+#1. Two Sum
+#difficulty - easy
+
+class Solution:
+    def twoSum(self, nums, target):
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
