03ceea8b8569ccf4a277a54b30e562501115fd51
Chama1010 <ashenchamath1010@GMAIL.com>
2026-01-15 14:18:06 +0530

    testing

--- arrays/01_two_sum.py
+++ arrays/01_two_sum.py
@@ -4,7 +4,7 @@
 class Solution:
     def twoSum(self, nums, target):
         n = len(nums)
-        for i in range(n):
+        for i in range(n): #d
             for j in range(i + 1, n):
                 if nums[i] + nums[j] == target:
                     return [i, j]
